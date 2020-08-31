c0737d9a86b55ea9539afebeda162bb525ecaa68
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-08-31 14:23:31 +0800

    checkpoint

--- a3c-test.py
+++ a3c-test.py
@@ -3,11 +3,11 @@ import ctypes
 import tensorflow as tf
 import numpy as np
 import tensorflow.compat.v1 as tfv1
-import models.A3C as A3C
 import models.A2C as A2C
 import models.expStrategy.epsilonGreedy as EPSG
 import envs.cartPole as cartPole
 
+tf.config.set_soft_device_placement(True)
 
 def init_agent_env():
     env = cartPole.CartPoleEnv()
@@ -47,19 +47,9 @@ def get_res(global_res_queue, global_alive_workers):
         
 
 def param_server(proc_id, ps_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queues):
-    # server = tf.distribute.Server(cluster_spec, role, role_index)
+    # server = tf.distribute.Server(cluster_spec, 'ps', ps_id)
     # with tf.device("/job:ps/task:0"):
     #     server.join()
-    # local_env = cartPole.CartPoleEnv()
-    # NUM_STATE_FEATURES = local_env.get_num_state_features()
-    # NUM_ACTIONS = local_env.get_num_actions()
-    # EPISODE_NUM = 20
-    # PRINT_EVERY_EPISODE = 20
-    # LEARNING_RATE = 0.003
-    # REWARD_DISCOUNT = 0.99
-    # data_type = tf.float32
-    # exp_stg = EPSG.EpsilonGreedy(0.2, NUM_ACTIONS)
-    # global_agent = A2C.Agent((NUM_STATE_FEATURES, ), NUM_ACTIONS, REWARD_DISCOUNT, LEARNING_RATE, exp_stg)
 
     global_agent, env = init_agent_env()
 
@@ -69,38 +59,25 @@ def param_server(proc_id, ps_id, global_remain_episode, global_alive_workers, gl
     # with tf.device("/job:localhost/replica:0/task:0/device:CPU:0"):
     with tf.device("/CPU:0"):
         while ((not global_grad_queue.empty()) or (global_alive_workers.value > 0)):
-            # if not global_grad_queue.empty():
-            # print(f'Getting gradients from queue')
+            print(f'Getting gradients from queue')
             item = global_grad_queue.get()
             global_agent.update(loss = item['loss'], gradients = item['gradients'])
 
             weights = global_agent.model.get_weights()
             for queue in global_var_queues:
                 if not queue.full():
-                    # out = queue.get_nowait()
-                    # print(out == None)
                     queue.put(weights)
-                    # print(f'Put vars in queue for worker')
+                    print(f'Put vars in queue for worker')
         
-        # print("Complete PS apply")
+        print("Complete PS apply")
         for queue in global_var_queues:
             if not queue.empty():
                 queue.get()
-                # print(f'Clear vars in queue for worker')
+                print(f'Clear vars in queue for worker')
 
-def worker(proc_id, worker_id, global_remain_episode, global_alive_workers, global_res_queue, global_grad_queue, global_var_queue):
+def worker(proc_id, worker_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queue):
+    # server = tf.distribute.Server(cluster_spec, 'worker', worker_id)
     print(f'Process {proc_id} Worker {worker_id} start')
-
-    # local_env = cartPole.CartPoleEnv()
-    # NUM_STATE_FEATURES = local_env.get_num_state_features()
-    # NUM_ACTIONS = local_env.get_num_actions()
-    # EPISODE_NUM = 20
-    # PRINT_EVERY_EPISODE = 20
-    # LEARNING_RATE = 0.003
-    # REWARD_DISCOUNT = 0.99
-    # data_type = tf.float32
-    # exp_stg = EPSG.EpsilonGreedy(0.2, NUM_ACTIONS)
-    # local_agent = A2C.Agent((NUM_STATE_FEATURES, ), NUM_ACTIONS, REWARD_DISCOUNT, LEARNING_RATE, exp_stg)
 
     local_agent, local_env = init_agent_env()
 
@@ -110,18 +87,16 @@ def worker(proc_id, worker_id, global_remain_episode, global_alive_workers, glob
     # with tf.device("/job:localhost/replica:0/task:0/device:CPU:0"):
     with tf.device("/CPU:0"):
         state = local_env.reset()
-        # for i in range(20):
         while global_remain_episode.value > 0:
             episode_reward, loss, gradients, trajectory = local_agent.train_on_env(env = local_env, cal_gradient_vars = None)
-            # local_agent.update(loss = loss, gradients = gradients)
-            print(f'Episode {global_remain_episode.value} Reward with process {worker_id}: {episode_reward}')
+            print(f'Episode {global_remain_episode.value} Reward with worker {worker_id}: {episode_reward}')
 
-            global_res_queue.put({'loss': loss, 'reward': episode_reward, 'worker_id': worker_id})
+            # global_res_queue.put({'loss': loss, 'reward': episode_reward, 'worker_id': worker_id})
             global_grad_queue.put({'loss': loss, 'gradients': gradients, 'worker_id': worker_id})
             if not global_var_queue.empty():
                 global_vars = global_var_queue.get()
                 local_agent.model.set_weights(global_vars)
-                # print(f'Worker {worker_id} Update Weights')
+                print(f'Worker {worker_id} Update Weights')
 
             with global_remain_episode.get_lock():
                 global_remain_episode.value -= 1
@@ -129,15 +104,19 @@ def worker(proc_id, worker_id, global_remain_episode, global_alive_workers, glob
     with global_alive_workers.get_lock():
         global_alive_workers.value -= 1
 
+    print(f"Worker {worker_id} exit")
+
 
 if __name__ == '__main__':
-    EPISODE_NUM = 20
+    # print(tf.config.experimental.list_physical_devices(device_type=None))
+    # print(tf.config.experimental.list_logical_devices(device_type=None))
+
+    EPISODE_NUM = 100
     ps_num = 1
-    worker_num = 2
-    # global_lock = Lock()
+    worker_num = 1
     global_remain_episode = Value('i', EPISODE_NUM)
     global_alive_workers = Value('i', worker_num)
-    global_res_queue = Queue()
+    # global_res_queue = Queue()
     global_grad_queue = Queue()
     global_var_queues = [Queue(1) for i in range(worker_num)]
     cluster_config = make_cluster_config(ps_num, worker_num)
@@ -151,7 +130,7 @@ if __name__ == '__main__':
         pss.append(Process(target=param_server, args=(ps_id, ps_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queues)))
 
     for worker_id in range(worker_num):
-        workers.append(Process(target=worker, args=(worker_id + ps_num, worker_id, global_remain_episode, global_alive_workers, global_res_queue, global_grad_queue, global_var_queues[worker_id])))
+        workers.append(Process(target=worker, args=(worker_id + ps_num, worker_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queues[worker_id])))
 
     for num in range(ps_num):
         pss[num].start()
