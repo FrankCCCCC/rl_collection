a9e2b2ba2c6e45a732b9bdb8c49c19cf8c7efad3
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-12-30 00:35:14 +0800

    add A3C-flappy.py

--- a3c-flappy.py
+++ a3c-flappy.py
@@ -3,26 +3,22 @@ import os
 import ctypes
 import tensorflow as tf
 import numpy as np
-# import tensorflow.compat.v1 as tfv1
+import pandas as pd
 import models.A2C as A2C
 import envs.cartPole as cartPole
 import envs.flappyBird as flappyBird
+import models.util as Util
 
-class Master:
+class A3C:
     def __init__(self):
         os.environ["CUDA_VISIBLE_DEVICES"] = "0"
         os.environ["OMP_NUM_THREADS"] = "1"
         os.environ["SDL_VIDEODRIVER"] = "dummy"
+        os.environ["SDL_AUDIODRIVER"] = "dummy"
         tf.config.set_soft_device_placement(True)
 
     def worker(self, proc_id, worker_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queue, global_res_queue):
-        # server = tf.distribute.Server(cluster_spec, 'worker', worker_id)
         print(f'Process {proc_id} Worker {worker_id} start')
-
-        # config = tf.compat.v1.ConfigProto()
-        # config.gpu_options.allow_growth=True   
-        # with tfv1.Session(target = server.target, config=config).as_default() as sess:
-        # with tf.device("/job:localhost/replica:0/task:0/device:CPU:0"):
 
         gpus = tf.config.experimental.list_physical_devices('GPU')
         tf.config.experimental.set_memory_growth(gpus[0], True)
@@ -35,7 +31,7 @@ class Master:
                 # print(f'Episode {global_remain_episode.value} Reward with worker {worker_id}: {episode_reward}')
 
                 global_res_queue.put({'loss': loss, 'reward': episode_reward, 'worker_id': worker_id})
-                global_grad_queue.put({'loss': loss, 'gradients': gradients, 'worker_id': worker_id})
+                global_grad_queue.put({'loss': loss, 'reward': episode_reward, 'gradients': gradients, 'worker_id': worker_id})
                 if not global_var_queue.empty():
                     global_vars = global_var_queue.get()
                     local_agent.model.set_weights(global_vars)
@@ -50,24 +46,28 @@ class Master:
         print(f"Worker {worker_id} done")
 
     def param_server(self, proc_id, ps_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queues):
-        # server = tf.distribute.Server(cluster_spec, 'ps', ps_id)
-
-        # config = tf.compat.v1.ConfigProto()
-        # config.gpu_options.allow_growth=True   
-        # with tfv1.Session(target = server.target, config=config).as_default() as sess:
-        # with tf.device("/job:localhost/replica:0/task:0/device:CPU:0"):
 
         gpus = tf.config.experimental.list_physical_devices('GPU')
         tf.config.experimental.set_memory_growth(gpus[0], True)
+
         # tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10240)])
         with tf.device("/CPU:0"):
             global_agent, env = self.init_agent_env(proc_id, 'ps', ps_id)
+
+            # Setup recorder
+            ckpt = tf.train.Checkpoint(model=global_agent.model, opt=global_agent.optimizer)
+            recorder = Util.Recorder(ckpt=ckpt, ckpt_path='results/ckpt', plot_title='A3C FlappyBird', filename='results/a3c_flappy', save_period=200)
+            ep = recorder.restore()
+            print(f"Restore {ep}")
+            with global_remain_episode.get_lock():
+                global_remain_episode.value = global_remain_episode.value - ep
 
             while ((not global_grad_queue.empty()) or (global_alive_workers.value > 0)):
                 if not global_grad_queue.empty():
                     # print(f'Getting gradients from queue')
                     item = global_grad_queue.get()
                     global_agent.update(loss = item['loss'], gradients = item['gradients'])
+                    recorder.record(float(item['loss']), float(item['reward']))
 
                     weights = global_agent.model.get_weights()
                     for i in range(len(global_var_queues)):
@@ -87,31 +87,13 @@ class Master:
         env = flappyBird.FlappyBirdEnv()
         NUM_STATE_FEATURES = env.get_num_state_features()
         NUM_ACTIONS = env.get_num_actions()
-        PRINT_EVERY_EPISODE = 20
-        LEARNING_RATE = 0.003
+        LEARNING_RATE = 0.0001
         REWARD_DISCOUNT = 0.99
         COEF_VALUE= 1
         COEF_ENTROPY = 0
         agent = A2C.Agent((NUM_STATE_FEATURES, ), NUM_ACTIONS, REWARD_DISCOUNT, LEARNING_RATE, COEF_VALUE, COEF_ENTROPY)
 
         return agent, env
-
-    def make_cluster_config(self, ps_num, worker_num, host = 'localhost', base_port = 8000):
-        cluster_config = {
-            'worker': [],
-            'ps': []
-        }
-        port = base_port
-
-        for ps_id in range(self.ps_num):
-            cluster_config['ps'].append(f'{host}:{port}')
-            port += 1
-
-        for worker_id in range(self.worker_num):
-            cluster_config['worker'].append(f'{host}:{port}')
-            port += 1
-        
-        return cluster_config
 
     # def is_having_training_info(self):
     #     return ((not global_res_queue.empty()) or (global_alive_workers.value > 0))
@@ -125,22 +107,19 @@ class Master:
         # print(tf.config.experimental.list_physical_devices(device_type=None))
         # print(tf.config.experimental.list_logical_devices(device_type=None))
 
-        self.episode_num = 200
+        self.episode_num = 100000
         self.ps_num = 1
-        self.worker_num = 2
+        self.worker_num = 25
         self.current_episode = 1
         global_remain_episode = Value('i', self.episode_num)
         global_alive_workers = Value('i', self.worker_num)
         global_res_queue = Queue()
         global_grad_queue = Queue()
         global_var_queues = [Queue(1) for i in range(self.worker_num)]
-        cluster_config = self.make_cluster_config(self.ps_num, self.worker_num)
 
         pss = []
         workers = []
         episode_results = []
-
-        cluster_spec = tf.train.ClusterSpec(cluster_config)
         
         for ps_id in range(self.ps_num):
             pss.append(Process(target = self.param_server, args=(ps_id, ps_id, global_remain_episode, global_alive_workers, global_grad_queue, global_var_queues)))
@@ -183,5 +162,5 @@ if __name__ == '__main__':
     # print(tf.config.experimental.list_physical_devices(device_type=None))
     # print(tf.config.experimental.list_logical_devices(device_type=None))
 
-    m = Master()
-    m.start()
+    a3c = A3C()
+    a3c.start()
--- models/util.py
+++ models/util.py
@@ -1,4 +1,8 @@
+import pandas as pd
+from IPython.display import display
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.pylab import figure
 
 def test_gpu():
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -15,3 +19,93 @@ def test_gpu():
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
+
+class Recorder():
+    def __init__(self, ckpt=None, ckpt_path=None, max_to_keep=250, plot_title=None, moving_avg_coef=0.04, filename=None, save_period=200):
+        self.df = pd.DataFrame(columns=['epoch', 'loss', 'avg_loss', 'reward', 'avg_reward'])
+        self.moving_avg_coef = moving_avg_coef
+        self.avg_loss = 0
+        self.avg_reward = 0
+        self.plot_title = plot_title
+        self.filename = filename
+        self.n = 0
+        self.save_period = save_period
+        self.is_checkpoint = (ckpt != None) and (ckpt_path != None)
+        if self.is_checkpoint:
+            self.ckpt = ckpt
+            self.ckpt_mang = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=max_to_keep)
+
+    def record(self, loss, reward):
+        new_avg_loss = (1 - self.moving_avg_coef) * self.avg_loss + self.moving_avg_coef * loss
+        new_avg_reward = (1 - self.moving_avg_coef) * self.avg_reward + self.moving_avg_coef * reward
+        self.avg_loss = new_avg_loss
+        self.avg_reward = new_avg_reward
+        
+        self.df = self.df.append({'epoch': self.n, 'loss': loss, 'avg_loss': new_avg_loss, 'reward': reward, 'avg_reward': new_avg_reward}, ignore_index=True)
+
+        if (self.filename and ((self.n % self.save_period == 0) or (self.n == 0))):
+            # print({'epoch': self.n, 'loss': loss, 'avg_loss': new_avg_loss, 'reward': reward, 'avg_reward': new_avg_reward})
+            # print(self.df)
+            self.to_csv()
+            self.to_plot()
+            if self.is_checkpoint:
+                self.ckpt_mang.save()
+
+        self.n = self.n + 1
+    
+    def to_csv(self):
+        self.df.to_csv(f"{self.filename}.csv")
+
+    def to_plot(self):
+        # Plot Reward History
+        # figure(num=None, figsize=(24, 6), dpi=80)
+        df = self.df.loc[self.df['epoch'] % 50 == 0]
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(24, 6), dpi=80)
+        fig.suptitle(f'{self.plot_title}')
+
+        ax1.plot(df['epoch'], df['reward'], color='blue', label='reward')
+        ax1.plot(df['epoch'], df['avg_reward'], color='red', label='avg reward')
+        ax1.set_xlabel('Episodes')
+        ax1.set_ylabel('Reward / Episode')
+        ax1.grid()
+        ax1.legend()
+
+        ax2.plot(df['epoch'], df['loss'], color='green', label='loss')
+        ax2.plot(df['epoch'], df['avg_loss'], color='orange', label='avg loss')
+        ax2.set_xlabel('Episodes')
+        ax2.set_ylabel('Loss / Episode')
+        ax2.grid()
+        ax2.legend()
+
+        plt.savefig(f"{self.filename}.svg")
+        plt.savefig(f"{self.filename}.png")
+        # plt.show()
+
+    def restore(self):
+        try:
+            if self.is_checkpoint:
+                latest = self.ckpt_mang.latest_checkpoint
+                print(f"latest {latest}")
+                if latest:
+                    self.ckpt.restore(latest)
+            df_old = pd.read_csv(f"{self.filename}.csv", index_col=0)
+            self.df = df_old
+            recover_ep = int(self.df['epoch'].iloc[-1])
+            
+            print(f"Recover from Record {recover_ep}")
+            self.n = recover_ep + 1
+
+            return recover_ep
+
+        except:
+            print("No CSV")
+            return 0
+
+    def display(self, head=None):
+        if not head:
+            display(self.df)
+        else:
+            display(self.df.head(head))
+
+    def get_dataframe(self):
+        return self.df
